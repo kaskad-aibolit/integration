b317e218a7df6430fe17a47ab873c9b37d285eaf
stepan <dysemir@gmail.com>
2024-06-20 22:00:18 +0300

    add api prefix

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -29,7 +29,7 @@ class RouteServiceProvider extends ServiceProvider
         });
 
         $this->routes(function () {
-            Route::middleware('api')
+            Route::middleware('integration/api')
                 ->prefix('api')
                 ->group(base_path('routes/api.php'));
 
